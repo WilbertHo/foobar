df009ca02365e715b9d98ac15db9b81c6f61a27a
Wilbert Ho <wilbert.ho@gmail.com>
2015-03-09 19:02:04 +1300

    [minglish lesson] (py) Initial.

---
+++ minglish_lesson/py/minglishlesson.py
@@ -0,0 +1,74 @@
+"""
+Minglish lesson
+===============
+
+Welcome to the lab, minion. Henceforth you shall do the bidding of
+Professor Boolean. Some say he's mad, trying to develop a zombie serum and
+all... but we think he's brilliant! 
+
+First things first - Minions don't speak English, we speak Minglish. Use
+the Minglish dictionary to learn! The first thing you'll learn is how to
+use the dictionary.
+
+Open the dictionary. Read the page numbers, figure out which pages come
+before others. You recognize the same letters used in English, but the
+order of letters is completely different in Minglish than English (a < b <
+c < ...).
+
+Given a sorted list of dictionary words (you know they are sorted because
+you can read the page numbers), can you find the alphabetical order of the
+Minglish alphabet? For example, if the words were ["z", "yx", "yz"] the
+alphabetical order would be "xzy," which means x < z < y. The first two
+words tell you that z < y, and the last two words tell you that x < z.
+
+Write a function answer(words) which, given a list of words sorted
+alphabetically in the Minglish alphabet, outputs a string that contains
+each letter present in the list of words exactly once; the order of the
+letters in the output must follow the order of letters in the Minglish
+alphabet. 
+
+The list will contain at least 1 and no more than 50 words, and each word
+will consist of at least 1 and no more than 50 lowercase letters [a-z]. It
+is guaranteed that a total ordering can be developed from the input
+provided (i.e. given any two distinct letters, you can tell which is
+greater), and so the answer will exist and be unique.
+
+Languages
+=========
+
+To provide a Python solution, edit solution.py
+To provide a Java solution, edit solution.java
+
+Test cases
+==========
+
+Inputs:
+    (string list) words = ["y", "z", "xy"]
+Output:
+    (string) "yzx"
+
+Inputs:
+    (string list) words = ["ba", "ab", "cb"]
+Output:
+    (string) "bac"
+"""
+
+
+def answer(words):
+    """ Given a sorted list of words, output the corresponding alphabet.
+
+        ["ba", "ab", "cb"]
+        Sliding window of 2 over the list:
+        (ba, ab) : '' + b, '' + a, create A, B vertexes, edge B -> A
+        (ab, cb) : '' + a, '' + c, create C vertex, edge A -> C
+        B -> A (B < A), A -> C (A < C) == BAC
+
+        [C, CAC, CB, BCC, BA]
+        (C, CAC) : '' + C, '' + C, create C and A vertexes
+        (CAC, CB) : C + A, C + B, create B vertex, edge A -> B (A < B)
+        (CB, BCC) : '' + C, '' + B, edge C -> B (C < B)
+        (BCC, BA) : B + C, B + A, edge C -> A (C < A)
+
+        C < A, A < B, C < B == CAB
+    """
+    pass
